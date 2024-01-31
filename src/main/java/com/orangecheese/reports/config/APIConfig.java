4231c6ca16007d0d8c4137e3e2534c8fb091d39a
Nosaiii <jcv.vanderlee@hotmail.com>
2024-01-31 19:16:47 +0100

    Added more API connection settings to the config.yml

--- src/main/java/com/orangecheese/reports/config/APIConfig.java
+++ src/main/java/com/orangecheese/reports/config/APIConfig.java
@@ -1,12 +1,34 @@
 package com.orangecheese.reports.config;
 
+import com.orangecheese.reports.core.http.HTTPProtocol;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class APIConfig {
+    private final HTTPProtocol protocol;
+
+    private final String hostName;
+
+    private final int port;
+
     private final int version;
 
     public APIConfig(ConfigurationSection section) {
-        this.version = section.getInt("version");
+        this.protocol = HTTPProtocol.valueOf(section.getString("protocol", "HTTPS").toUpperCase());
+        this.hostName = section.getString("host-name", "reportsplugin.com");
+        this.port = section.getInt("port", 443);
+        this.version = section.getInt("version", 1);
+    }
+
+    public HTTPProtocol getProtocol() {
+        return protocol;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public int getPort() {
+        return port;
     }
 
     public int getVersion() {
